27a9c0a641573b75fa033927f65bd695635c119b
unknown <3j36matsuo@gmail.com>
2020-01-14 09:41:42 +0900

    ：

--- cgi-bin/webled.py
+++ cgi-bin/webled.py
@@ -9,24 +9,24 @@ import RPi.GPIO as GPIO
 print('Content-type: text/html; charset=UTF-8\r\n')
 print('Web LED')
 
-#print('<form action="" method="post">')
-#print('<input type="submit" name="led" value="ON">')
-#print('<input type="submit" name="led" value="OFF">')
-#print('</form>')
+print('<form action="" method="post">')
+print('<input type="submit" name="led" value="ON">')
+print('<input type="submit" name="led" value="OFF">')
+print('</form>')
 
 
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(14, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
 
-#form = cgi.FieldStorage()
-#value = form.getvalue("led")
+form = cgi.FieldStorage()
+value = form.getvalue("led")
 
-#if value == 'ON':
-#	GPIO.output(14, GPIO.HIGH)
-#	print('LED ON')
+if value == 'ON':
+	GPIO.output(14, GPIO.HIGH)
+	print('LED ON')
 
-#elif value == 'OFF':
-#	GPIO.output(14, GPIO.LOW)
-#	print('LED OFF')
+elif value == 'OFF':
+	GPIO.output(14, GPIO.LOW)
+	print('LED OFF')
 
 
--- cgi-bin/webservo.py
+++ cgi-bin/webservo.py
@@ -6,31 +6,31 @@ import time
 import RPi.GPIO as GPIO
 
 
-def setservo(range):
-	if -90.0<=range<=90.0:
-		duty=2.5+(range+90)*(12.0-2.5)/180
-		servo.ChangeDutyCycle(duty)
+#def setservo(range):
+#	if -90.0<=range<=90.0:
+#		duty=2.5+(range+90)*(12.0-2.5)/180
+#		servo.ChangeDutyCycle(duty)
 		
 		
 print('Content-type: text/html; charset=UTF-8\r\n')
 print('Web Servo')
 
-print('<form action="" method="post">')
-print('<p>')
-print('AngleÅF')
-print('<input type="number" name="angle" value="0">')
-print('</p>')
-print('<input type="submit" value="submit">')
-print('</form>')
+#print('<form action="" method="post">')
+#print('<p>')
+#print('AngleÅF')
+#print('<input type="number" name="angle" value="0">')
+#print('</p>')
+#print('<input type="submit" value="submit">')
+#print('</form>')
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(2, GPIO.OUT)
 
-form = cgi.FieldStorage()
-angle = form.getvalue("angle")
+#form = cgi.FieldStorage()
+#angle = form.getvalue("angle")
 
-servo = GPIO.PWM(2, 50)
-servo.start(float(angle))
+#servo = GPIO.PWM(2, 50)
+#servo.start(float(angle))
 
 
