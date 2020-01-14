d4c1f9516632c621305bac0c2c9ce0427e439016
unknown <3j36matsuo@gmail.com>
2020-01-14 09:50:21 +0900

    ：

--- cgi-bin/webservo.py
+++ cgi-bin/webservo.py
@@ -2,8 +2,31 @@
 
 import cgi
 import cgitb    #display CGI error on browser
+import time
+import RPi.GPIO as GPIO
+
 
 print('Content-type: text/html; charset=UTF-8\r\n')
-print('Web Servo')
+print('Web LED')
+
+print('<form action="" method="post">')
+print('<input type="submit" name="led" value="ON">')
+print('<input type="submit" name="led" value="OFF">')
+print('</form>')
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+
+form = cgi.FieldStorage()
+value = form.getvalue("led")
+
+if value == 'ON':
+	GPIO.output(14, GPIO.HIGH)
+	print('LED ON')
+
+elif value == 'OFF':
+	GPIO.output(14, GPIO.LOW)
+	print('LED OFF')
 
 
